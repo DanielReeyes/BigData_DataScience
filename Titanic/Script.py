6793daed929e4b13043460bf082108f044e0b7c5
daniel reyes <danielreyes@MacBook-Pro-de-daniel.local>
2018-03-05 23:14:19 -0300

    Adicionados dados de Teste, funcao de predicao de rotulo de sobrevivencia, funcao de acuracia e impressao de matriz de confusao

--- Titanic/Script.py
+++ Titanic/Script.py
@@ -3,8 +3,12 @@ import pandas as pd
 import os
 from sklearn import preprocessing
 from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
+import matplotlib.pyplot as plt
+import itertools
 
-os.chdir('/Users/danielreyes/Documents/ProjetosBigData/Titanic/CSVs') #Setando o caminho do arquivo de treino
+os.chdir('/Users/danielreyes/Documents/BigData_DataScience/Titanic/CSVs') #Setando o caminho do arquivo de treino
 Dados_Treino = pd.read_csv("train.csv") #Lendo o arquivo para guardar os valores
 
 #print(Dados_Treino.head(3))
@@ -29,25 +33,33 @@ RNA = MLPClassifier()
 #print(RNA)
 
 # Make data frame of predictors
-variaveis = pd.DataFrame([rotulo_sexo, 
-                          Dados_Treino["Pclass"],
-                          Dados_Treino["Age"],
-                          Dados_Treino["Fare"]]
-                         ).T
+#variaveis = pd.DataFrame([rotulo_sexo, 
+#                          Dados_Treino["Pclass"],
+#                          Dados_Treino["Age"],
+#                          Dados_Treino["Fare"]]
+#                         ).T
+
+variaveis = pd.DataFrame(Dados_Treino, columns=['Pclass','Age','Fare'])
+variaveis['Sexo'] = rotulo_sexo
+
+print("Amostra do novo DataFrame de Treino.: ")
+print(variaveis.head())
 
 RNA.fit(X=variaveis,
         y=Dados_Treino["Survived"])
 
+print("Score do Treino.: ")
 print(RNA.score(X=variaveis,
         y=Dados_Treino["Survived"]))
 
 #DADOS DE TESTE
 Dados_Teste = pd.read_csv("test.csv")    # Read the data
 
-# Impute median Age for NA Age values
-new_age_var = np.where(Dados_Teste["Age"].isnull(), # Logical check
-                       28,                       # Value if check is true
-                       Dados_Teste["Age"])      # Value if check is false
+#Normalizando os dados de teste
+#Cria uma nova coluna, quando a idade não foi informada (Null), passará a idade média dos dados (28)
+new_age_var = np.where(Dados_Teste["Age"].isnull(), # teste se for nulo
+                       28,                          # Valor se for nulo
+                       Dados_Teste["Age"])          # se não for nulo, pega a idade 
 
 Dados_Teste["Age"] = new_age_var 
 
@@ -55,7 +67,59 @@ Dados_Teste["Age"] = new_age_var
 # Convert test variables to match model features
 encoded_sex_test = codificador_rotulos.fit_transform(Dados_Teste["Sex"])
 
-variaveis_teste = pd.DataFrame([encoded_sex_test,
-                              Dados_Teste["Pclass"],
-                              Dados_Teste["Age"],
-                              Dados_Teste["Fare"]]).T
+#variaveis_teste = pd.DataFrame([encoded_sex_test,
+#                              Dados_Teste["Pclass"],
+#                              Dados_Teste["Age"],
+#                              Dados_Teste["Fare"]]).T
+
+variaveis_teste = pd.DataFrame(Dados_Teste, columns=['Pclass','Age','Fare'])
+variaveis_teste['Sexo'] = encoded_sex_test
+
+print("Amostra do novo DataFrame de Teste.: ")
+print(variaveis_teste.head())
+
+rotulos_teste = pd.read_csv("gender_submission.csv")
+rotulos_teste = rotulos_teste["Survived"]
+
+variaveis_teste["Fare"] = np.where(variaveis_teste["Fare"].isnull(), # teste se for nulo
+                       0,                                            # Valor se for nulo
+                       variaveis_teste["Fare"])                      # se não for nulo, pega o valor de Fare 
+
+predicoes = RNA.predict(variaveis_teste)
+accuracy = accuracy_score(rotulos_teste, predicoes)
+
+print("Resultado dos dados de Teste.: ")
+print(accuracy)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('Rótulo Real')
+    plt.xlabel('Rótulo Predito')
+
+# Computando matriz de confusão
+cnf_matrix = confusion_matrix(rotulos_teste, predicoes)
+np.set_printoptions(precision=2)
+
+# Plotando a matriz de confusão
+class_names = ['Sobreviveu', 'N-Sobreviveu']
+plot_confusion_matrix(cnf_matrix, classes=class_names,
+                      title='Confusion matrix')
