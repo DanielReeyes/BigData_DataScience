a00d4a244b827ea9c1f55369d8031726105200e4
daniel reyes <danielreyes@MacBook-Pro-de-daniel.local>
2018-03-06 19:23:31 -0300

    Identação do código e adição de comentários

--- Titanic/Script.py
+++ Titanic/Script.py
@@ -1,96 +1,101 @@
 import numpy as np
 import pandas as pd
 import os
+import itertools
+import matplotlib.pyplot as plt
 from sklearn import preprocessing
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
-import matplotlib.pyplot as plt
-import itertools
 
-os.chdir('/Users/danielreyes/Documents/BigData_DataScience/Titanic/CSVs') #Setando o caminho do arquivo de treino
-Dados_Treino = pd.read_csv("train.csv") #Lendo o arquivo para guardar os valores
-
-#print(Dados_Treino.head(3))
+#Setando o caminho dos arquivos de dados
+os.chdir('/Users/danielreyes/Documents/BigData_DataScience/Titanic/CSVs')
+#Lendo o arquivo para guardar os dados de treino em um DataFrame
+Dados_Treino = pd.read_csv("train.csv") 
 
 #Normalizando os dados de treino
 #Cria uma nova coluna, quando a idade não foi informada (Null), passará a idade média dos dados (28)
-colMediaIdade = np.where(Dados_Treino["Age"].isnull(),
+colIdadeNormTreino = np.where(Dados_Treino["Age"].isnull(),
                          28,
                          Dados_Treino["Age"])
 
 #Substitui a coluna original dos dados pela coluna nova, sem dados faltantes
-Dados_Treino["Age"] = colMediaIdade
+Dados_Treino["Age"] = colIdadeNormTreino
 
+#Transformando os valores da coluna sexo de string para inteiro
 codificador_rotulos = preprocessing.LabelEncoder()
-rotulo_sexo = codificador_rotulos.fit_transform(Dados_Treino["Sex"])
+sexo_cod_treino = codificador_rotulos.fit_transform(Dados_Treino["Sex"])
 
+#Declara um modelo de Rede Neural Multi-Camada
 RNA = MLPClassifier()
-
-#RNA.fit(X=pd.DataFrame(rotulo_sexo),
-#        y=Dados_Treino["Survived"])
 
 #print(RNA)
 
-# Make data frame of predictors
-#variaveis = pd.DataFrame([rotulo_sexo, 
-#                          Dados_Treino["Pclass"],
-#                          Dados_Treino["Age"],
-#                          Dados_Treino["Fare"]]
-#                         ).T
-
-variaveis = pd.DataFrame(Dados_Treino, columns=['Pclass','Age','Fare'])
-variaveis['Sexo'] = rotulo_sexo
+#Criando um dataframe para treinar o modelo somente com as colunas escolhidas e
+# adicionando a coluna sexo normalizada
+Variaveis_Treino = pd.DataFrame(Dados_Treino, columns=['Pclass','Age','Fare'])
+Variaveis_Treino['Sexo'] = sexo_cod_treino
 
 print("Amostra do novo DataFrame de Treino.: ")
-print(variaveis.head())
+print(Variaveis_Treino.head())
 
-RNA.fit(X=variaveis,
+#Treinando o modelo RNA com os dados de treino para que ele descubra ligações
+#dos dados de treino com o rótulo de sobreviver
+RNA.fit(X=Variaveis_Treino,
         y=Dados_Treino["Survived"])
 
+#Mensurando a capacidade de acerto do modelo treinado
 print("Score do Treino.: ")
-print(RNA.score(X=variaveis,
+print(RNA.score(X=Variaveis_Treino,
         y=Dados_Treino["Survived"]))
 
 #DADOS DE TESTE
-Dados_Teste = pd.read_csv("test.csv")    # Read the data
+#Lendo o arquivo para guardar os dados de teste em um DataFrame
+Dados_Teste = pd.read_csv("test.csv")
 
 #Normalizando os dados de teste
 #Cria uma nova coluna, quando a idade não foi informada (Null), passará a idade média dos dados (28)
-new_age_var = np.where(Dados_Teste["Age"].isnull(), # teste se for nulo
-                       28,                          # Valor se for nulo
-                       Dados_Teste["Age"])          # se não for nulo, pega a idade 
+colIdadeNormTeste = np.where(Dados_Teste["Age"].isnull(),   # teste se for nulo
+                       28,                                  # Valor se for nulo
+                       Dados_Teste["Age"])                  # se não for nulo, pega a idade 
 
-Dados_Teste["Age"] = new_age_var 
+#Substitui a coluna original dos dados pela coluna nova, sem dados faltantes
+Dados_Teste["Age"] = colIdadeNormTeste 
 
+Dados_Teste["Fare"] = np.where(Dados_Teste["Fare"].isnull(), # teste se for nulo
+                       0,                                            # Valor se for nulo
+                       Dados_Teste["Fare"])                      # se não for nulo, pega o valor de Fare 
 
-# Convert test variables to match model features
-encoded_sex_test = codificador_rotulos.fit_transform(Dados_Teste["Sex"])
+#Transformando os valores da coluna sexo de string para inteiro
+sexo_cod_teste = codificador_rotulos.fit_transform(Dados_Teste["Sex"])
 
-#variaveis_teste = pd.DataFrame([encoded_sex_test,
-#                              Dados_Teste["Pclass"],
-#                              Dados_Teste["Age"],
-#                              Dados_Teste["Fare"]]).T
-
+#Criando um dataframe de teste somente com as colunas escolhidas e
+# adicionando a coluna sexo normalizada
 variaveis_teste = pd.DataFrame(Dados_Teste, columns=['Pclass','Age','Fare'])
-variaveis_teste['Sexo'] = encoded_sex_test
+variaveis_teste['Sexo'] = sexo_cod_teste
 
 print("Amostra do novo DataFrame de Teste.: ")
 print(variaveis_teste.head())
 
+#Como os rótulos dos dados de teste estão em um outro CSV, é necessário lê-los 
+#e guardar também em um array ou DataFrame para podermos comparar com as predições
+#feitas
 rotulos_teste = pd.read_csv("gender_submission.csv")
 rotulos_teste = rotulos_teste["Survived"]
 
-variaveis_teste["Fare"] = np.where(variaveis_teste["Fare"].isnull(), # teste se for nulo
-                       0,                                            # Valor se for nulo
-                       variaveis_teste["Fare"])                      # se não for nulo, pega o valor de Fare 
-
+#Utiliza o modelo RNA declarado e treinado na parte superior do código para 
+#efetuar predições com os dados de teste que não possuem vinculo algum 
+#com os seus rótulos
 predicoes = RNA.predict(variaveis_teste)
+
+#Utiliza uma função para contabilizar a acurácia, para isso precisa passar como
+#parâmetro os rótulos originais e os preditos
 accuracy = accuracy_score(rotulos_teste, predicoes)
 
 print("Resultado dos dados de Teste.: ")
 print(accuracy)
 
+#Função criada para plotar imagem da matriz de confusão e uma escala de cores
 def plot_confusion_matrix(cm, classes,
                           normalize=False,
                           title='Confusion matrix',
@@ -112,6 +117,8 @@ def plot_confusion_matrix(cm, classes,
                  color="white" if cm[i, j] > thresh else "black")
 
     plt.tight_layout()
+    
+    #rótulos dos eixos 'x' e 'y'
     plt.ylabel('Rótulo Real')
     plt.xlabel('Rótulo Predito')
 
