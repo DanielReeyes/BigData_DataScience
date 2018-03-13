3089220c7fe8a96d9718587e3c5a5759e57fa3dd
daniel reyes <danielreyes@MacBook-Pro-de-daniel.local>
2018-03-13 15:18:09 -0300

    Adicionadas funcionalidades de cross validation, smart grid search, métricas e matriz de confusao

--- Cancer/ClassificacaoCancer.py
+++ Cancer/ClassificacaoCancer.py
@@ -12,15 +12,42 @@ import os
 import itertools
 import matplotlib.pyplot as plt
 from sklearn import preprocessing
-from sklearn.neural_network import MLPClassifier
 from sklearn import tree
-from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import ShuffleSplit
-from sklearn.model_selection import cross_val_score
+from sklearn.metrics import classification_report
 
+#Função criada para plotar imagem da matriz de confusão e uma escala de cores
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues, 
+                          score=""):
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    #plt.tight_layout()
+    plt.show()
+    print(plt)
+    
+    #rótulos dos eixos 'x' e 'y'
+    plt.ylabel('Rótulo Real')
+    plt.xlabel('Rótulo Predito')
+        
 #Setando o caminho dos arquivos de dados
 os.chdir('/Users/danielreyes/Documents/BigData_DataScience/Cancer/CSVs')
 #Lendo o arquivo para guardar os dados em um DataFrame
@@ -31,69 +58,52 @@ rotulo = codificador_rotulos.fit_transform(df["Label"])
 
 df["Label"] = rotulo
 
-histogram_example = plt.hist(df["Label"])
-plt.show()
+#histogram_example = plt.hist(df["Label"])
+#plt.show()
 
 colunas = list(df)
 scaler = preprocessing.StandardScaler().fit(df)
     
 df = pd.DataFrame(scaler.transform(df), columns=colunas)
-#df["Label"] = rotulo
 del df["Label"]
 
-#df_treino, df_teste, rotulo_treino, rotulo_teste = train_test_split(
-#     df, rotulo, test_size=0.2, random_state=0)
+parametros = {'max_depth': range(3, 10)}
+tree = tree.DecisionTreeClassifier()
 
-parameters = {'max_depth': range(3, 10)}
-Modelo_Arvore = tree.DecisionTreeClassifier()
-clf = GridSearchCV(Modelo_Arvore, parameters)
+df_treino, df_teste, rotulo_treino, rotulo_teste = train_test_split(
+     df, rotulo, test_size=0.1, random_state=0)
 
-cv = ShuffleSplit(n_splits=10, test_size=0.2, random_state=0)
-scores = cross_val_score(clf, df, rotulo, cv=cv)
+# Set the parameters by cross-validation
 
-print("Acurácia: %0.2f (+/- %0.2f)" % (scores.mean(), scores.std() * 2))
+scores = ['precision', 'recall']
 
-#Treinando o modelo
-#DecisionTree.fit(Treino, target_treino)
+for score in scores:
+    print()
+    print("Métrica.: " + score)
+    print()
+
+    clf = GridSearchCV(tree, parametros, cv=10,
+                       scoring='%s' % score)
+    clf.fit(df_treino, rotulo_treino)
+
+    print("Melhores parâmetros.: {} " .format(clf.best_params_))
+
+    means = clf.cv_results_['mean_test_score']
+    stds = clf.cv_results_['std_test_score']
+
+    for mean, std, params in zip(means, stds, clf.cv_results_['params']):
+        if(clf.best_params_ == params):
+            print("%0.3f (+/-%0.03f) for %r"
+              % (mean, std * 2, params))
+
+    y_true, y_pred = rotulo_teste, clf.predict(df_teste)
+#    print(classification_report(y_true, y_pred))
+#    print()
+    # Computando matriz de confusão
+    cnf_matrix = confusion_matrix(y_true, y_pred)
+    np.set_printoptions(precision=2)
     
-#prediz resultados com o modelo treinado
-#predicted = DecisionTree.predict(Teste)
-
-
-
-
-#Função criada para plotar imagem da matriz de confusão e uma escala de cores
-#def plot_confusion_matrix(cm, classes,
-#                          normalize=False,
-#                          title='Confusion matrix',
-#                          cmap=plt.cm.Blues):
-#    print(cm)
-#
-#    plt.imshow(cm, interpolation='nearest', cmap=cmap)
-#    plt.title(title)
-#    plt.colorbar()
-#    tick_marks = np.arange(len(classes))
-#    plt.xticks(tick_marks, classes, rotation=45)
-#    plt.yticks(tick_marks, classes)
-#
-#    fmt = '.2f' if normalize else 'd'
-#    thresh = cm.max() / 2.
-#    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
-#        plt.text(j, i, format(cm[i, j], fmt),
-#                 horizontalalignment="center",
-#                 color="white" if cm[i, j] > thresh else "black")
-#
-#    plt.tight_layout()
-#    
-#    #rótulos dos eixos 'x' e 'y'
-#    plt.ylabel('Rótulo Real')
-#    plt.xlabel('Rótulo Predito')
-#
-## Computando matriz de confusão
-#cnf_matrix = confusion_matrix(rotulos_teste, predicoes)
-#np.set_printoptions(precision=2)
-#
-## Plotando a matriz de confusão
-#class_names = ['Sobreviveu', 'N-Sobreviveu']
-#plot_confusion_matrix(cnf_matrix, classes=class_names,
-#                      title='Confusion matrix')
+    # Plotando a matriz de confusão
+    class_names = ['Cancer', 'N-Cancer']
+    plot_confusion_matrix(cnf_matrix, classes=class_names,
+                          title='Confusion matrix', score=score)
